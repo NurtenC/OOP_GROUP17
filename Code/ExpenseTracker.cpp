290a66a7874e5287a6bd9f2be55dcf47d2642e06
furkaninkosesi <furkandemir1809@gmail.com>
2024-12-26 05:05:33 +0300

    fix ExpenseTracker Class

--- Code/ExpenseTracker.cpp
+++ Code/ExpenseTracker.cpp
@@ -1,78 +1,147 @@
-#include "ExpenseTable.h"
+#include "ExpenseTracker.h"
 
-ExpenseTable::ExpenseTable(int x, int y, int w, int h, std::vector<Expense> *expenses) : Fl_Table_Row(x, y, w, h, "Expense Table")
+ExpenseTracker::ExpenseTracker()
 {
-    this->expenses = expenses;
-    rows(10);
-    cols(4);
-    col_header(1);
-    col_resize(1);
-    row_height_all(25);
-    col_width(0, 100);
-    col_width(1, 100);
-    col_width(2, 100);
-    col_width(3, 250);
-    end();
+
+    currentUser = nullptr;
+    expenses = nullptr;
+    time = std::chrono::system_clock::now();
+    date = new Date(time);
 }
 
-void ExpenseTable::draw_cell(TableContext tableContext, int row, int col, int x, int y, int w, int h)
+ExpenseTracker::~ExpenseTracker()
 {
-    if (expenses == nullptr)
+
+    delete date;
+}
+
+bool ExpenseTracker::userExists(const std::string &username) const
+{
+
+    return users.find(username) != users.end();
+}
+
+bool ExpenseTracker::registerUser(const std::string &username, const std::string &password, double budget)
+{
+
+    if (userExists(username))
     {
-        if (tableContext == CONTEXT_CELL)
-        {
-            fl_color(FL_WHITE);
-            fl_rectf(x, y, w, h);
-            fl_color(FL_BLACK);
-            fl_draw("No data", x + 5, y, w, h, FL_ALIGN_CENTER);
-        }
-        return;
+
+        return false;
     }
-
-    std::string text;
-
-    switch (tableContext)
-    {
-    case CONTEXT_STARTPAGE:
-        fl_font(FL_HELVETICA, 14);
-        break;
-    case CONTEXT_COL_HEADER: // regularExpense->color(fl_rgb_color(0x96, 0x7e, 0xd7));
-        static const char *headers[] = {"Date", "Category", "Amount", "Description"};
-        fl_draw_box(FL_THIN_UP_BOX, x, y, w, h, FL_WHITE);
-        fl_color(FL_BLACK);
-        fl_draw(headers[col], x + 5, y, w, h, FL_ALIGN_LEFT);
-        break;
-    case CONTEXT_CELL:
-        fl_color(row_selected(row) ? FL_YELLOW : FL_WHITE);
-        fl_rectf(x, y, w, h);
-
-        fl_color(FL_BLACK);
-        switch (col)
-        {
-        case 0:
-            text = (*expenses)[row].getDate().getDateString();
-            break;
-        case 1:
-            text = (*expenses)[row].getCategoryAsString();
-            break;
-        case 2:
-            text = (*expenses)[row].getAmountAsString();
-            break;
-        case 3:
-            text = (*expenses)[row].getDescription();
-            break;
-        }
-        fl_draw(text.c_str(), x + 5, y, w, h, FL_ALIGN_LEFT);
-        break;
-    default:
-        break;
-    }
+    users.emplace(username, User(username, password, budget));
+    return true;
 }
 
-void ExpenseTable::updateTable(std::vector<Expense> *expenses)
+bool ExpenseTracker::login(const std::string &username, const std::string &password)
 {
-    this->expenses = expenses;
-    int a = expenses ? expenses->size() : 0;
-    rows(a);
-    redraw();
+
+    auto iterator = users.find(username);
+    if (iterator != users.end() && iterator->second.checkPassword(password))
+    {
+
+        currentUser = &(iterator->second);
+        return true;
+    }
+    return false;
 }
+
+void ExpenseTracker::logout()
+{
+    currentUser = nullptr;
+}
+
+std::string ExpenseTracker::getCurrentUsername() const
+{
+    if (currentUser)
+    {
+        return currentUser->getUsername();
+    }
+    return "";
+}
+
+std::string ExpenseTracker::getBudgetAsString() const
+{
+    if (currentUser)
+    {
+        return currentUser->getBudgetAsString();
+    }
+    return "";
+    
+}
+
+Date *ExpenseTracker::getDate()
+{
+
+    return date;
+}
+
+std::chrono::system_clock::time_point ExpenseTracker::getTime() const
+{
+
+    return time;
+}
+
+std::vector<Expense> *ExpenseTracker::getExpenses()
+{
+    if (currentUser)
+    {
+        return currentUser->getExpenses();
+    }
+    return nullptr;
+}
+
+std::vector<Income> *ExpenseTracker::getIncomes()
+{
+    if (currentUser)
+    {
+        return currentUser->getIncomes();
+    }
+    return nullptr;
+}
+
+void ExpenseTracker::updateTime(int time)
+{
+    this->time += std::chrono::seconds(time);
+}
+bool ExpenseTracker::updateCurrentUserInfo(std::string& username, const std::string& password, double budget){
+    
+    currentUser->setUsername(username);
+    currentUser->setPassword(password);
+    currentUser->setBudget(budget);
+    return true;
+}
+void ExpenseTracker::updateDate(std::chrono::system_clock::time_point time)
+{
+
+    delete date;
+    date = new Date(time);
+}
+
+void ExpenseTracker::addExpense(Expense expense)
+{
+    currentUser->addExpense(expense);
+}
+
+void ExpenseTracker::addIncome(Income income)
+{
+    currentUser->addIncome(income);
+}
+
+Category ExpenseTracker::getCategory(int index)
+{
+    return categories[index];
+}
+
+std::array<Category, 10> ExpenseTracker::categories = {
+    Category::GROCERY,
+    Category::HEALTH,
+    Category::ENTERTAINMENT,
+    Category::EDUCATION,
+    Category::TRANSPORTATION,
+    Category::PERSONAL_CARE,
+    Category::CLOTHES,
+    Category::TAX,
+    Category::BILL,
+    Category::RENT,
+};

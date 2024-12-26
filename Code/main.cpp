61f05b8f02a1dae29d095e3e541096da581e1500
furkaninkosesi <furkandemir1809@gmail.com>
2024-12-26 23:55:37 +0300

    Doxygen Main Page

--- Code/main.cpp
+++ Code/main.cpp
@@ -1,4 +1,22 @@
-
+/**
+ * @mainpage Expense Tracker Application
+ *
+ * @section intro Introduction
+ * The Expense Tracker is an application designed to manage personal finances.
+ * It allows users to track incomes and expenses, view summaries, and generate reports.
+ *
+ * @section features Features
+ * - Manage incomes and expenses
+ * - Generate detailed financial reports
+ * - Easy-to-use graphical user interface
+ *
+ * @section usage Usage
+ * Compile code with make
+ * Launch the application.
+ * Add incomes and expenses.
+ * View financial summaries and reports.
+ *
+ */
 
 #include "GuiManager.h"
 #include "ExpenseTracker.h"
